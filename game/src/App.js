ccf7d29556df5af73bf9b36ef3c3dec653ef03de
Zbdj <45045776+Zbdj@users.noreply.github.com>
2019-12-08 18:56:11 +0100

    Get Data From Server

--- game/src/App.js
+++ game/src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import axios from 'axios';
 
 export default class App extends Component{
+  state={
+    word : "",
+    traduction : "",
+    difficult : 0,
+    score: 10,
+  }
+  componentDidMount(){
+    this.getWord();
+  }
+
+  getWord(){
+    axios.get('http://localhost:4000/', {
+    }).then(rep => {
+      //console.log(rep.data)
+
+      this.setState({
+        word:rep.data.word,
+        traduction: rep.data.result,
+        difficult: rep.data.difficulty
+      })
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Ton score : {this.state.score}</p>
+          <p>Mot : {this.state.word}</p>
+          <p>Trad : {this.state.traduction}</p>
+          <p>Difficulté : {this.state.difficult} / 10</p>
         </header>
       </div>
     );
