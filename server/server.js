120c080a558693a23ae80855ab8601c38332b851
Zbdj <45045776+Zbdj@users.noreply.github.com>
2019-12-08 18:23:03 +0100

    Connect to YandexAPI

--- server/server.js
+++ server/server.js
@@ -4,9 +4,25 @@ const app = express();
 var cors = require('cors')
 app.use(cors());
 
+const axios = require('axios')
 
 app.get('/', function (req, res) {
-    res.json("Hello World");
+
+    axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate', {
+        params: {
+          key: "trnsl.1.1.20191207T163533Z.3220a5c776decfc5.2ee94ae14fc425c44edc17a3a2e6bc996f916eb5",
+          text: "Bonjour Monde",
+          lang: 'en'
+        }
+      }).then(rep => {
+        //console.log(rep.data)
+        var object = {
+            "word": "Bonjour Monde",
+            "result": rep.data.text[0],
+        }
+
+        res.json(object)
+      }) 
 })
 
 console.log("Port 4000")
