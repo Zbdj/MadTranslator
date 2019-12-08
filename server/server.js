e0bf035f95463470ac828005ff82695309d5c255
Zbdj <45045776+Zbdj@users.noreply.github.com>
2019-12-08 18:37:54 +0100

    RandomWord With Translate Done

--- server/server.js
+++ server/server.js
@@ -6,23 +6,49 @@ app.use(cors());
 
 const axios = require('axios')
 
-app.get('/', function (req, res) {
+const MongoClient = require('mongodb').MongoClient;
 
-    axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate', {
-        params: {
-          key: "trnsl.1.1.20191207T163533Z.3220a5c776decfc5.2ee94ae14fc425c44edc17a3a2e6bc996f916eb5",
-          text: "Bonjour Monde",
-          lang: 'en'
-        }
-      }).then(rep => {
-        //console.log(rep.data)
-        var object = {
-            "word": "Bonjour Monde",
-            "result": rep.data.text[0],
-        }
+MongoClient.connect("mongodb+srv://Zbdj:Rw0pM71iWMGy1r7T@cluster0-zed6f.mongodb.net/test?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}, function (err, db) {
+  if (!err) {
+    console.log("Connection To DataBase");
+  }
 
-        res.json(object)
-      }) 
+  if (err) throw err;
+
+  var dbo = db.db("MadWord");
+  var RandomWord = "";
+  var Note = 5;
+
+    app.get('/', function (req, res) {
+        
+        dbo.collection("MadWord").find({difficulty : Note}).toArray(function(err,result) {
+            //console.log(result)
+            RandomWord = result[Math.floor(Math.random() * result.length)].word;
+            //console.log(RandomWord)
+
+            axios.get('https://translate.yandex.net/api/v1.5/tr.json/translate', {
+                params: {
+                key: "trnsl.1.1.20191207T163533Z.3220a5c776decfc5.2ee94ae14fc425c44edc17a3a2e6bc996f916eb5",
+                text: RandomWord,
+                lang: 'en'
+                }
+            }).then(rep => {
+                //console.log(rep.data)
+                var object = {
+                    "word": RandomWord,
+                    "result": rep.data.text[0],
+                    "difficulty":Note,
+                }
+    
+                res.json(object)
+            }) 
+        })
+
+
+    })
 })
 
 console.log("Port 4000")
